41d1b83a1a2ae9daaccf8ca46952101171c5cf1d
John Colton <jcolton96@gmail.com>
2024-10-29 17:30:35 +0000

    output instance id

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
 output "factorio_server_connection_string" {
   value = "${aws_instance.factorio_server.public_ip}:${local.factorio_port}"
 }
+
+output "factorio_server_instance_id" {
+  value = aws_instance.factorio_server.id
+}
